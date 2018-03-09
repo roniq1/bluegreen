6dfb79060658fe50393e9e495f894ba141e9d6eb
roniq1 <szalkk@gmail.com>
2018-03-09 16:09:37 +0100

    Update image.php
    Resize the rectangle

--- image.php
+++ image.php
@@ -14,7 +14,7 @@ if ( $deployment == 'blue') {
 }
 
 // Draw a filled rectangle
-imagefilledrectangle($im, 0, 0, 199, 199, $color);
+imagefilledrectangle($im, 0, 0, 240, 240, $color);
 
 // Save the image
 header('Content-Type: image/png');
